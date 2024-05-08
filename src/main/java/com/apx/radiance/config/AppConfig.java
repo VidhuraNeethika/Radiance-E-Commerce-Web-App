8aceadd6958951144ce20652e613e4e07c1b35f9
VidhuraNeethika <vidhuraneethika000@gmail.com>
2024-05-08 08:59:19 +0530

    Update comments

--- src/main/java/com/apx/radiance/config/AppConfig.java
+++ src/main/java/com/apx/radiance/config/AppConfig.java
@@ -7,13 +7,13 @@ import org.glassfish.jersey.server.mvc.jsp.JspMvcFeature;
 public class AppConfig extends ResourceConfig {
 
     public AppConfig(){
-        packages("com.apx.radiance.controller");
+        packages("com.apx.radiance.controller"); // package to scan for resources
         packages("com.apx.radiance.middleware");
 
-        register(JspMvcFeature.class);
-        register(MultiPartFeature.class);
-        property(JspMvcFeature.TEMPLATE_BASE_PATH,"/WEB-INF/views");
-        register(DependencyBinder.class);
+        register(JspMvcFeature.class); // register JSP MVC feature. it is required to use JSP as view template engine in the application.
+        register(MultiPartFeature.class); // register MultiPartFeature to enable file upload feature in the application.
+        property(JspMvcFeature.TEMPLATE_BASE_PATH,"/WEB-INF/views"); // set the base path of JSP files
+        register(DependencyBinder.class); // register DependencyBinder to bind the dependencies
     }
 
 }
